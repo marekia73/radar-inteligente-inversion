32877fb063d071fc21d71eb0b7138a79a2b84116
marekia73 <rafaeltroyagarcia@gmail.com>
2026-05-20 15:59:48 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 async function startServer() {
   const app = express();
   app.use(cors({ origin: true }));
-  const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   // Endpoint FRED
   app.get("/api/fred", async (req, res) => {
